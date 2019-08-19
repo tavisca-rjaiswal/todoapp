14da059a130187a5fe7f819931b1fdb4653c7b82
Jaiswal <rjaiswal@internal.tavisca.com>
2019-08-20 00:27:27 +0530

    Todoapp

---
+++ js/render.js
@@ -0,0 +1,35 @@
+var todoDisplay=document.getElementById("todoDisplay");
+var userDisplay=document.getElementById("userDisplay");
+var loginDisplay=document.getElementById("loginDisplay");
+
+var todo=document.getElementById("todo");
+var user=document.getElementById("user");
+var login=document.getElementById("login");
+
+userDisplay.style.display="none";
+loginDisplay.style.display="none";
+
+var DispTodo = () =>
+{
+    todoDisplay.style.display="block";
+    userDisplay.style.display="none";
+    loginDisplay.style.display="none";
+}
+var DispUser = () => 
+{
+    todoDisplay.style.display="none";
+    userDisplay.style.display="block";
+    loginDisplay.style.display="none";
+}
+var DispLogin = () =>
+{
+    todoDisplay.style.display="none";
+    userDisplay.style.display="none";
+    loginDisplay.style.display="block";
+}
+
+todo.addEventListener("click",DispTodo);
+user.addEventListener("click",DispUser);
+login.addEventListener("click",DispLogin);
+
+DispUser();
